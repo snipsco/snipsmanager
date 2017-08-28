a909b4e5f8152790eb2afadc764f655bdae47fc0
anthonyray <dantho361@hotmail.com>
2017-08-28 18:10:10 +0200

    moving templating system to Jinja2 ✨

--- snipsskills/commands/scaffold.py
+++ snipsskills/commands/scaffold.py
@@ -1,10 +1,9 @@
 # -*-: coding utf-8 -*-
 import os
+from jinja2 import Environment, PackageLoader
 
 from .base import Base
 from ..utils.os_helpers import create_dir_verbose, write_text_file_verbose
-from ..templates.scaffolding import README_TEMPLATE, SETUP_TEMPLATE, LICENSE_TEMPLATE, MANIFEST_TEMPLATE, LINT_TEMPLATE, SETUP_CONFIG_TEMPLATE
-
 
 from snipsskillscore.logging import log, log_success, log_error
 
@@ -32,13 +31,15 @@ class Scaffold(Base):
     """
     def __init__(self, skill_name):
         self.project_name = skill_name
+        self.jinja_env = Environment(
+            loader=PackageLoader('snipsskills', 'templates'))
 
     def run(self):
         project_name = self.project_name
         current_directory = os.getcwd()
 
         try:
-            print "Scaffolding {} structure".format(project_name)clear
+            print "Scaffolding {} structure".format(project_name)
             self.create_folders(project_name, current_directory)
             self.create_files(project_name, current_directory)
 
@@ -75,7 +76,8 @@ class Scaffold(Base):
     def write_setup(self, project_name, root_directory):
         setup_path = os.path.join(root_directory, 'setup.py')
 
-        setup_content = SETUP_TEMPLATE.format(project_name, project_name, project_name)
+        SETUP_template = self.jinja_env.get_template('setup.py')
+        setup_content = SETUP_template.render(project_name=project_name)
         write_text_file_verbose(setup_path, setup_content, 1)
 
     def write_snips_spec(self, project_name, root_directory):
@@ -97,26 +99,33 @@ class Scaffold(Base):
     def write_readme(self, project_name, root_directory):
         readme_path = os.path.join(root_directory, 'README.rst')
 
-        README_content = README_TEMPLATE.format(project_name)
+        README_template = self.jinja_env.get_template('README.rst')
+        README_content = README_template.render(project_name=project_name)
         write_text_file_verbose(readme_path, README_content, 1)
 
 
     def write_license(self, project_name, root_directory):
         license_path = os.path.join(root_directory, 'LICENSE.txt')
-        write_text_file_verbose(license_path, LICENSE_TEMPLATE, 1)
+        LICENSE_template = self.jinja_env.get_template('LICENSE.txt')
+
+        write_text_file_verbose(license_path, LICENSE_template.render(), 1)
 
     def write_manifest(self, project_name, root_directory):
         manifest_path = os.path.join(root_directory, 'MANIFEST.in')
 
-        manifest_content = MANIFEST_TEMPLATE.format(project_name)
+        MANIFEST_template = self.jinja_env.get_template('MANIFEST.in')
+        manifest_content = MANIFEST_template.render(project_name)
         write_text_file_verbose(manifest_path, manifest_content, 1)
 
     def write_configs(self, project_name, root_directory):
         lint_path = os.path.join(root_directory, 'lint.cfg')
         setup_path = os.path.join(root_directory, 'setup.cfg')
 
-        lint_content = LINT_TEMPLATE.format()
-        setup_content = SETUP_CONFIG_TEMPLATE.format()
+        LINT_template = self.jinja_env.get_template('lint.cfg')
+        SETUP_CONFIG_template = self.jinja_env.get_template('setup.cfg')
+
+        lint_content = LINT_template.render()
+        setup_content = SETUP_CONFIG_template.render()
 
         write_text_file_verbose(lint_path, lint_content, 1)
         write_text_file_verbose(setup_path, setup_content, 1)
