24d0910bd29b29932e6734e55ecb31731168e28e
anthonyray <dantho361@hotmail.com>
2017-08-28 18:25:29 +0200

    moving to Jinja templating system

---
+++ snipsskills/templates/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='',
+    version='0.1.4.5.5',
+    description='Fake weather forescasts skill for Snips',
+    author='Michael Fester',
+    author_email='michael.fester@gmail.com',
+    url='https://github.com/snipsco/snips-skill-fakeweather',
+    download_url='',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['green'],
+    keywords=['snips'],
+    include_package_data=True,
+    packages=[
+        'snipsfakeweather'
+    ]
+)
