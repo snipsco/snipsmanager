bfdf46799a702bd34d6399ddaadc5f81de04fec3
anthonyray <dantho361@hotmail.com>
2017-09-01 13:47:30 +0200

    login backend

--- snipsskills/utils/assistant_downloader.py
+++ snipsskills/utils/assistant_downloader.py
@@ -1,20 +1,39 @@
 # -*-: coding utf-8 -*-
 """ Downloader for Snips assistants. """
 
+from http_helpers import post_request_json
 import os
 import json
-from http_helpers import post_request_json
+import re
 
 try:
     from urllib.request import urlopen
 except ImportError:
     from urllib2 import urlopen, Request, URLError
 
-USER_AUTH_ROUTE = "https://private-gateway-dev.snips.ai/v1/user/auth"
+USER_AUTH_ROUTE = "https://private-gateway.snips.ai/v1/user/auth"
+
+
+class AuthException(Exception):
+    pass
+
+
+class AuthExceptionInvalidCredentials(AuthException):
+    pass
+
+
+class AuthExceptionInvalidAssistantId(AuthException):
+    pass
+
 
 class DownloaderException(Exception):
     """ Exceptions related to downloads of Snips assistants. """
     pass
+
+
+def email_is_valid(email):
+    return True if re.match(r"[^@]+@[^@]+\.[^@]+", email) else False
+
 
 # pylint: disable=too-few-public-methods
 class Downloader(object):
@@ -53,13 +72,26 @@ class Downloader(object):
 
 
 class AuthDownloader(Downloader):
-    def __init__(self, email, password):
+    def __init__(self, email, password, assistantId):
         self.auth_url = USER_AUTH_ROUTE
         self.email = email
         self.password = password
+        self.assistantId = assistantId
+        self.validate_input()
+        self._DOWNLOAD_URL = "https://private-gateway.snips.ai/v1/assistant/{}/download".format(assistantId)
+
+    def validate_input(self):
+        if not email_is_valid(self.email):
+            raise AuthExceptionInvalidCredentials("Error, Email is not valid")
+
+        if len(self.password) < 1:
+            raise AuthExceptionInvalidCredentials("Error, password is too short")
+
+        if len(self.assistantId) < 14:
+            raise AuthExceptionInvalidAssistantId("Error, assistantId is too short")
 
     def retrieve_auth_token(self):
-        data = {'email': self.email, 'password': self.password }
+        data = {'email': self.email, 'password': self.password}
 
         try:
             response, response_headers = post_request_json(self.auth_url, data)
@@ -68,19 +100,14 @@ class AuthDownloader(Downloader):
         except URLError:
             raise DownloaderException
 
-
-    def download(self, url, output_dir, filename):
+    def download(self, output_dir, filename):
         try:
             token = self.retrieve_auth_token()
-            request = Request(url, headers={'Authorization': token})
+            request = Request(self._DOWNLOAD_URL, headers={'Authorization': token, 'Accept': 'application/json'})
             response = urlopen(request)
-            print response
         except Exception:
             raise DownloaderException()
 
         Downloader.save(response.read(),
                         output_dir,
                         filename)
-
-
-
